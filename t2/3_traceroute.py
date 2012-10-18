1866b1d3074cefdec15ccc152db1dddab87139f3
eMancu <emiliano.mancuso@gmail.com>
2012-10-18 20:14:56 -0300

    [T2] Fix traceroute and add attempts

--- t2/3_traceroute.py
+++ t2/3_traceroute.py
@@ -2,42 +2,37 @@
 
 from scapy.all import *
 
-
 def tracerouteICMP(host):
-	ipPkg = IP(dst=host,ttl=1) / ICMP() / 'test del tp'
-	noTimeOut = True
-	noRoute = True
-	i =1
-	attempts = 0
-	while noRoute and noTimeOut :
-		
-		print "ttl = %d" %ipPkg.ttl
-		response = sr1(ipPkg, timeout=20, verbose=0)
-		if not (response is None):
-			response.show()
-			#miro la direccion origen de la respuesta
-			respSrc = response.src
-			if (respSrc == host): #HABRIA QUE MIRAR EL TIPO?? SI ES ECHO-REPLY??
-				noRoute = False
-				print "Destino: " + respSrc
-			#miro el tipo de respuestas ICMP
-			#echo replay, por ahora encontre la ruta
-			respType = response.payload.type
-			if respType==1 :
-				noRoute = False
-			#por ahora los otros tpos de replay los tomamos similar al time exceeded
-			if noRoute :
-				print "Ruta: " + respSrc
-				i = i + 1
-				ipPkg.ttl = i
-		else:
-			i = i + 1
-			ipPkg.ttl = i
-			print "Timeout waiting"
-			#noTimeOut = False  
-			
-	if not (noTimeOut)	:
-		print "Timeout waiting"
-		
-		
+  ipPkg = IP(dst=host,ttl=1) / ICMP() / 'test del tp'
+  noTimeOut = True
+  noRoute = True
+  attempts = 3
+  while noRoute:
+    print "ttl = %d" %ipPkg.ttl
+
+    response = sr1(ipPkg, timeout=2, verbose=0)
+
+    if (response is None):
+      attempts -= 1
+      if (attempts == 0):
+        attempts = 3
+        ipPkg.ttl += 1
+        print " * "
+
+    else:
+      # miro la direccion origen de la respuesta
+      respSrc = response.src
+      if (respSrc == host): #HABRIA QUE MIRAR EL TIPO?? SI ES ECHO-REPLY??
+        noRoute = False
+        print "Destino: " + respSrc
+      # miro el tipo de respuestas ICMP
+      # echo replay, por ahora encontre la ruta
+      respType = response.payload.type
+      if respType==1 :
+        noRoute = False
+      # por ahora los otros tpos de replay los tomamos similar al time exceeded
+      if noRoute :
+        print "Ruta: " + respSrc
+        ipPkg.ttl += 1
+
 tracerouteICMP(sys.argv[1])
