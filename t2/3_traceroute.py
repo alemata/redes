1c27e635390fa585d534611e86cc83e6f17d3424
eMancu <emiliano.mancuso@gmail.com>
2012-10-26 17:24:38 -0300

    Change ICMP type to *echo replay*

--- t2/3_traceroute.py
+++ t2/3_traceroute.py
@@ -9,7 +9,6 @@ def tracerouteICMP(host):
   while noRoute:
     print "ttl = %d" %ipPkg.ttl
 
-    #response = sr1(ipPkg, timeout=2, verbose=0)
     response = sr1(ipPkg, verbose=0, timeout=3, retry=3, inter=1)
 
     if (response is None):
@@ -19,15 +18,15 @@ def tracerouteICMP(host):
     else:
       # miro la direccion origen de la respuesta
       respSrc = response.src
-      if (respSrc == host): #HABRIA QUE MIRAR EL TIPO?? SI ES ECHO-REPLY??
+      if (respSrc == host):
         noRoute = False
         print "Destino: " + respSrc
-      # miro el tipo de respuestas ICMP
-      # echo replay, por ahora encontre la ruta
-      respType = response.payload.type
-      if respType == 1 : # Segun el curtu aca va un 0
+
+      # miro el tipo de respuestas ICMP y busco un echo replay
+      if response.payload.type == 0 :
         noRoute = False
-      # por ahora los otros tpos de replay los tomamos similar al time exceeded
+        # por ahora los otros tpos de replay los tomamos similar al time exceeded
+
       if noRoute :
         print "Ruta: " + respSrc
         ipPkg.ttl += 1
