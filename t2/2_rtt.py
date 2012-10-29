f7d6875717690e82f73633ed21447ca1bc0b12af
Alejandro Mataloni <amataloni@gmail.com>
2012-10-29 14:51:25 -0300

    Agregando promedio al rtt

--- t2/2_rtt.py
+++ t2/2_rtt.py
@@ -5,11 +5,14 @@
 from scapy.all import *
 from Ping import ping
 
-
+total = 0;
 for x in range(int(sys.argv[2])):
 	time = ping(sys.argv[1]);
 	if time != -1:
+		total = total + time;
 		print "Time = %s" % time	
+print total;
+print total / int(sys.argv[2])
 
 
 
